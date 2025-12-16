80f460a55ad145584e1869e616e2301db77985f5
SamuelMedina07 <semedina@uth.hn>
2025-12-16 14:24:11 -0600

    Add Sort by name and release date

--- src/com/aluracursos/principal/PrincipalConListas.java
+++ src/com/aluracursos/principal/PrincipalConListas.java
@@ -5,6 +5,8 @@ import com.aluracursos.screenmatch.modelos.Serie;
 import com.aluracursos.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalConListas {
     public static void main(String[] args) {
@@ -22,7 +24,7 @@ public class PrincipalConListas {
         lista.add(peliculaSamuel);
         lista.add(miPelicula);
         lista.add(otraPelicula);
-        lista.add(casaDragon);
+       
         for(Titulo Item: lista){
             System.out.println(Item);
             //Casting para poder entrar al metodo de clasificacion en Peliucas
@@ -30,5 +32,39 @@ public class PrincipalConListas {
             System.out.println(pelicula.getCalificacion());
 
         }
+
+        Pelicula favorita = new Pelicula("El señor de los anillos", 2001);
+        favorita.evalua(10);
+
+        Pelicula otra = new Pelicula("John Wick", 2014);
+        otra.evalua(9);
+
+        Serie serie = new Serie("La Casa de Papel", 2017);
+
+        ArrayList<Titulo> lista2 = new ArrayList<>();
+        lista2.add(favorita);
+        lista2.add(otra);
+        lista2.add(serie);
+
+        for(Titulo item : lista2) {
+            System.out.println("Nombre: " +item.getNombre());
+            if (item instanceof Pelicula pelicula && pelicula.getCalificacion() > 2) {
+                System.out.println("Clasificacion: " +pelicula.getCalificacion());
+            }
+        }
+
+        ArrayList<String> listaDeArtistas = new ArrayList<>();
+        listaDeArtistas.add("Penelope Cruz");
+        listaDeArtistas.add("Antonio Banderas");
+        listaDeArtistas.add("Ricardo Darin");
+
+        Collections.sort(listaDeArtistas);
+        System.out.println("Lista de Artistas Ordenadas:" + listaDeArtistas);
+
+        lista2.sort(Comparator.comparing(Titulo::getFechaDeLanzamiento));
+        System.out.println("Lista ordenada por fecha:"+lista2);
     }
+
+
+
 }

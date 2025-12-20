82ba247dfa5e51cc35b3bd1c642e3417737422e4
SamuelMedina07 <semedina@uth.hn>
2025-12-19 19:40:59 -0600

    Add guardando datos en formato JSON

--- src/com/aluracursos/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/principal/PrincipalConBusqueda.java
@@ -13,52 +13,69 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {
-    //indentacion ctrl + alt + I
+        //indentacion ctrl + alt + I
         Scanner lectura = new Scanner(System.in);
-        System.out.println("Escriba el nombre de la pelicula...:");
-        var busqueda = lectura.nextLine();
+        //interfaz y luego la clase
+        List<Titulo> titulos = new ArrayList<>();
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
 
-        String direccion = "https://www.omdbapi.com/?t=" +
-                busqueda.replace(" ","+")
-                + "&apikey=2b83c21c";
-        try{
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(direccion))
-                    .build();
 
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
+        while (true) {
 
-            String json = response.body();
-            System.out.println(json);
+            System.out.println("Escriba el nombre de la pelicula...:");
+            var busqueda = lectura.nextLine();
 
-            // Gson gson = new Gson();
-            //Titulo miTitulo = gson.fromJson(json, Titulo.class);
+            if(busqueda.equalsIgnoreCase("salir")){break;}
 
-            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-            TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-            System.out.println(miTituloOmdb);
+            String direccion = "https://www.omdbapi.com/?t=" +
+                    busqueda.replace(" ", "+")
+                    + "&apikey=2b83c21c";
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(direccion))
+                        .build();
 
-            Titulo miTitulo = new Titulo(miTituloOmdb);
-            System.out.println("Titulo ya convertido: "+miTitulo);
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
 
-            FileWriter escritura = new FileWriter("peliculas.txt");
-            escritura.write(miTitulo.toString());
-            escritura.close();
+                String json = response.body();
+                System.out.println(json);
+
+                // Gson gson = new Gson();
+                //Titulo miTitulo = gson.fromJson(json, Titulo.class);
+
+
+                TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(miTituloOmdb);
+
+                Titulo miTitulo = new Titulo(miTituloOmdb);
+                System.out.println("Titulo ya convertido: " + miTitulo);
+
+                titulos.add(miTitulo);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Ocurrio un error...:");
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error en la URI, verifique la direccion.");
+            } catch (ErrorEnConversionDeDuracionException e) {
+                System.out.println(e.getMessage());
+            }
         }
-        catch(NumberFormatException e) {
-            System.out.println("Ocurrio un error...:");
-            System.out.println(e.getMessage());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error en la URI, verifique la direccion.");
-        } catch (ErrorEnConversionDeDuracionException e){
-            System.out.println(e.getMessage());
-        }
+        System.out.println(titulos);
+        FileWriter escritura = new FileWriter("titulos.json");
+        escritura.write(gson.toJson(titulos));
+        escritura.close();
         System.out.println("Finalizo la ejecucion del programa");
 
 
--- src/com/aluracursos/screenmatch/modelos/Titulo.java
+++ src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -5,9 +5,8 @@ import com.google.gson.annotations.SerializedName;
 
 public class Titulo implements Comparable <Titulo> {
 
-    @SerializedName("Title")
+
         private String nombre;
-    @SerializedName("Year")
         private int fechaDeLanzamiento;
         private int duracionEnMinutos;
         private boolean incluidoEnElPlan;
@@ -88,8 +87,8 @@ public class Titulo implements Comparable <Titulo> {
     @Override
     public String toString() {
         return
-                "nombre='" + nombre + '\'' +
+                "(nombre=" + nombre  +
                 ", fechaDeLanzamiento=" + fechaDeLanzamiento+
-                ", duracion=" + duracionEnMinutos;
+                ", duracion=" + duracionEnMinutos + ")";
     }
 }

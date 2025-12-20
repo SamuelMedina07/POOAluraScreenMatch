a4307ab6f66fba28bed162a2e056aa5dd68ff0ea
SamuelMedina07 <semedina@uth.hn>
2025-12-19 19:13:49 -0600

    Add guardando datos en txt pero se sobreescriben con FileWrite

--- src/com/aluracursos/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/principal/PrincipalConBusqueda.java
@@ -7,6 +7,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -45,6 +46,10 @@ public class PrincipalConBusqueda {
 
             Titulo miTitulo = new Titulo(miTituloOmdb);
             System.out.println("Titulo ya convertido: "+miTitulo);
+
+            FileWriter escritura = new FileWriter("peliculas.txt");
+            escritura.write(miTitulo.toString());
+            escritura.close();
         }
         catch(NumberFormatException e) {
             System.out.println("Ocurrio un error...:");

10078f8ff46f99872539d59ee81eaa8340ddec4f
SamuelMedina07 <semedina@uth.hn>
2025-12-19 18:29:59 -0600

    Add clase dos de try y catch basicos sin resolver errores

--- src/com/aluracursos/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/principal/PrincipalConBusqueda.java
@@ -15,37 +15,39 @@ import java.util.Scanner;
 
 public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {
-
+    //indentacion ctrl + alt + I
         Scanner lectura = new Scanner(System.in);
         System.out.println("Escriba el nombre de la pelicula...:");
         var busqueda = lectura.nextLine();
 
         String direccion = "https://www.omdbapi.com/?t=" + busqueda + "&apikey=2b83c21c";
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(direccion))
-                .build();
-
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        String json = response.body();
-        System.out.println(json);
-
-       // Gson gson = new Gson();
-        //Titulo miTitulo = gson.fromJson(json, Titulo.class);
-
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(miTituloOmdb);
         try{
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(direccion))
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+
+            // Gson gson = new Gson();
+            //Titulo miTitulo = gson.fromJson(json, Titulo.class);
+
+            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+            TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(miTituloOmdb);
+
             Titulo miTitulo = new Titulo(miTituloOmdb);
-            System.out.println(miTitulo);
+            System.out.println("Titulo ya convertido: "+miTitulo);
         }
         catch(NumberFormatException e) {
             System.out.println("Ocurrio un error...:");
             System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error en la URI, verifique la direccion.");
         }
         System.out.println("Finalizo la ejecucion del programa");
 
